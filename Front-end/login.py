7dd95522c6e35182fba858a0cde564185619c38a
SKY <sky012877@gmail.com>
2023-04-12 10:58:58 -0700

    Front-end/login.py - Modified :

--- Front-end/login.py
+++ Front-end/login.py
@@ -1,15 +1,42 @@
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
+from selen import Selen
 from time import sleep
+from security import EMAIL, PASSW
 
 
+class Login(Selen):
 
-class Account:
-    
+    # locators
+    l_login_btn = ("class name", "Navigation_login__JL_4K")
+    l_login_field = ("xpath", "//body/div[@id='fb-root']/div[@id='root']/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/input[1]")
+    l_password_field = ('xpath', "//body/div[@id='fb-root']/div[@id='root']/div[1]/div[1]/div[2]/div[1]/form[1]/div[2]/div[1]/input[1]")
+    l_login_btn_wrapper = ("class name", "Login_submit_wrapper__2-PYe")
+    l_login_submit_btn = ("tag name", "button")
+
+    # l_pag_href = ("tag name", "a")
+    # l_tbody = ("tag name", "tbody")
+    # l_tr = ("tag name", "tr")
+
     def __init__(self):
-        self.drv = webdriver.Chrome()
+        super().__init__()
+        self.url = "https://ibench.net/"
+
+        # locators
+        #l_login_btn = ("class", "Navigation_login__JL_4K")
+
+        # l_pag_href = ("tag name", "a")
+        # l_tbody = ("tag name", "tbody")
+        # l_tr = ("tag name", "tr")
+
+
+    def main(self):
+        self.WD.get(self.url)
+        self.wait_click_to(self.l_login_btn)
+        sleep(1)
+        self.wait_text_to(EMAIL, self.l_login_field)
+        self.text_to(PASSW, self.l_password_field)
+        self.click_login_btn()
+        sleep(100)
+
+
+if __name__ == "__main__":
+    Login().main()
