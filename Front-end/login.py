863d86e61c9cda72ab61d83b15efb200a5600c5f
SKY <sky012877@gmail.com>
2023-04-12 15:57:21 -0700

    Front-end/login.py - Modified :

--- Front-end/login.py
+++ Front-end/login.py
@@ -1,6 +1,7 @@
 from selen import Selen
 from time import sleep
 from security import EMAIL, PASSW
+import unittest
 
 
 class Login(Selen):
@@ -13,14 +14,15 @@ class Login(Selen):
     l_login_submit_btn_wrapper = ("class name", "Login_submit_wrapper__2-PYe")
     l_login_submit_btn = ("tag name", "button")
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, wd):
+        super().__init__(wd)
         self.url = "https://ibench.net/"
 
     # def
 
     def main(self):
         self.WD.get(self.url)
+        print("\n")
         self.check_title("iBench - real-time developers Hiring")
 
         self.wait_click_to(self.l_login_btn)
@@ -31,8 +33,46 @@ class Login(Selen):
         self.text_to(PASSW, self.l_password_field)
         self.click_to(self.l_login_submit_btn_wrapper, self.l_login_submit_btn)
         self.save_cookies_to_file("security.py")
-        sleep(100)
+        sleep(10)
+        self.WD.close()
+
+
+class Chrome_Login(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.cls = Login("Chrome")
+
+    def test_login(self):
+        self.cls.main()
+
+    def tearDown(self) -> None:
+        self.cls = None
+
+
+class Firefox_Login(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.cls = Login("Firefox")
+
+    def test_login(self):
+        self.cls.main()
+
+    def tearDown(self) -> None:
+        self.cls = None
+
+
+class Edge_Login(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.cls = Login("Edge")
+
+    def test_login(self):
+        self.cls.main()
+
+    def tearDown(self) -> None:
+        self.cls = None
 
 
 if __name__ == "__main__":
-    Login().main()
+    pass
+    # Login().main()
