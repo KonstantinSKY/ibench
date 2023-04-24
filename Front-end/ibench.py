2b066977286df20c9b01ee1782fd37ccc95f5157
SKY <sky012877@gmail.com>
2023-04-23 23:31:28 -0700

    Front-end/ibench.py - Modified :

--- Front-end/ibench.py
+++ Front-end/ibench.py
@@ -27,62 +27,89 @@ class iBench(Selen):
 
     def main_page(se):
         se.WD.get(se.url)  # Get page from WD
-        # New Wait method
-
         # Wait element and check inner text
         se.Wait(l_h1).text('Looking for a developers, UX/UI designer, QA or DevOps...or development agency?')
-
-        se.Tag("head").out("head:")
-        se.Cls('FrontPage_clientImage__3KW8O').text().out("Image Page")
-
-        se.Tag('h1').text().out("Text of element:")
-        se.Tag('h1').xpath_query().out()
-
         se.title('iBench - real-time developers Hiring')  # Check title
-        se.title().out("Page title:")  # Output of page title
-
-        se.Cls('FrontPage_btnWrapper__2Q75S').out("Wrapper element:")
-
-        se.curr_url().out("URL:")  # Output URL
         se.curr_url("https://ibench.net/")  # Check url
-<<<<<<< HEAD
-        # se.check_links(asynchron=True)
-=======
-        # se.check_links(asynchron=False).stat.out("Link Statistic")
->>>>>>> cce5c6de178f0b4265858631baa65e2e90eed5fd
+        se.Img(check=True).stat.out()  # Check all Images
+        se.check_links(asynchron=True).sleep(0, 3)  # Check all Links
 
+        # se.Tag("head").out("head:")
+        # se.Cls('FrontPage_clientImage__3KW8O').text().out("Image Page")
+        # se.Tag('h1').text().out("Text of element:")
+        # se.Tag('h1').xpath_query().out()
+        # se.title().out("Page title:")  # Output of page title
+        # se.Cls('FrontPage_btnWrapper__2Q75S').out("Wrapper element:")
+        # se.curr_url().out("URL:")  # Output URL
+        # se.check_links(asynchron=False).stat.out("Link Statistic")
         # Checking images on the page
         # se.Img(check=True).sleep(10)  # Check all Images
-        se.Img(check=True).stat.out()  # Check all Images
 
     def login(se):
-        lc_submit_button = ((CLASS, "Login_submit_wrapper__2-PYe"), (TAG, "button"))
+        lc_submit_button = ((CLASS, "Login_submit_wrapper__2-PYe"), (TAG, "button"))  # Submit button locator
 
-        se.main_page()
-        se.Contains("Log in").click()
-<<<<<<< HEAD
-        #se.Wait(l_h2).text("Log in")
-        #se.Img(check=True)
-=======
-        #se.Wait(l_h2).text("Login").click()
-
->>>>>>> cce5c6de178f0b4265858631baa65e2e90eed5fd
+        se.main_page()  # Start main page
+        se.Contains("Log in").click().Wait(l_h2).text("Log in")  # Click to login and wait for new page header with tex
         se.curr_url("https://ibench.net/login").title("Log in | iBench - real-time developers Hiring")
+        se.Img(check=True).check_links()
 
+        # Enter email and password with checking for update 'value' attribute and green checkmarks
         se.Find(NAME, "email").type(EMAIL).sleep(0.2).attr('value', EMAIL).out()
         se.parent(2).tag("span").attr('class', 'validation_status_ok')
         se.Find(NAME, "email").xpath_query().out("XPath")
         se.Find(NAME, "password").type(PASSW).sleep(0.2).attr('value', PASSW)
         se.parent(2).tag("span").attr('class', 'validation_status_ok')
 
-        se.Find(*lc_submit_button).click()
-        # se.sleep(10)
+        # se.Find(*lc_submit_button).click().sleep(2)  #click submit button
+        se.Tag('button').click().sleep(2, 4)
+
         se.Wait(l_h1).text("Daily updates")
-        se.curr_url("https://ibench.net/stats")
-        se.title("Daily updates | iBench - real-time developers Hiring")
+        se.curr_url("https://ibench.net/stats").title("Daily updates | iBench - real-time developers Hiring")
+        se.Img(check=True).check_links()
         sleep(10)
 
+        # se.Find(NAME, "email").xpath_query().out("XPath")
+        # se.Wait(l_h2).text("Log in")
+        # se.Img(check=True)
+        # se.Wait(l_h2).text("Login").click()
 
+    def about(se):
+        se.Wait(l_h1).text("""iBench is an easy hiring way to find new highly-skilled 
+                              Remote developers in 2-3 business days after your request.)""")
+        se.curr_url('https://ibench.net/about').title('About | iBench - real-time developers Hiring')
+        se.Img(check=True).check_links()
+
+    def hire_remote_team(se):
+        se.Wait(l_h1).text('About | iBench - real-time developers Hiring')
+        se.curr_url('https://ibench.net/team-search').title('About | iBench - real-time developers Hiring')
+        se.Img(check=True).check_links()
+
+        page_data = {'about': {'curr_url': 'https://ibench.net/about',
+                               'title': 'About | iBench - real-time developers Hiring',
+                               'locator': """(l_h1).text(iBench is an easy hiring way to find new highly-skilled 
+                                              Remote developers in 2-3 business days after your request.)"""
+                               },
+                     'Hire remote team': {
+                                'curr_url': 'https://ibench.net/team-search',
+                                # 'title': 'team-search | iBench - real-time developers Hiring',
+                                'title': 'About | iBench - real-time developers Hiring',
+                                'locator': '(l_h1).text("Hire remote developers team")'
+                                },
+                     # 'Blog': {
+                     #     'curr_url': 'https://ibench.net/team-search',
+                     #     # 'title': 'team-search | iBench - real-time developers Hiring',
+                     #     'title': 'About | iBench - real-time developers Hiring',
+                     #     'h1_text': "Hire remote developers team"
+                     # },
+                     # 'Home': {
+                     #     'curr_url': 'https://ibench.net/',
+                     #     # 'title': 'team-search | iBench - real-time developers Hiring',
+                     #     'title': 'About | iBench - real-time developers Hiring',
+                     #     'h1_text': "Hire remote developers team"
+                     # },
+
+                     }
+        for elem in page_data:
 
     def login_cookies(self):
         self.add_cookies()
